72cf45936c824eca9d6a9ba9323d69b1eb393fb0
Christian <quedarme@gmail.com>
2017-03-09 20:16:40 +0100

    Add files via upload

---
+++ spider-man/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+   $('#forname').click(function(){
+      $('#name').show(800);
+   });
+
+   $('#foremail').click(function(){
+      $('#email').show(800);
+   });
+
+   $('#forphone').click(function(){
+      $('#phone').show(800);
+   });
+
+   $('#formessage').click(function(){
+      $('#message').show(800);
+   });
+
+});
